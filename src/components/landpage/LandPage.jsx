4bbf5d6c6fff3689d14abc41c8f178fd83914c67
urielbang <uri32655@gmail.com>
2024-03-31 00:24:55 +0300

    link to cv

--- src/components/landpage/LandPage.jsx
+++ src/components/landpage/LandPage.jsx
@@ -1,7 +1,7 @@
 import NavBar from "../navbar/NavBar";
 import Spline from "@splinetool/react-spline";
 import { load } from "./loaded";
-import { FaReact } from "react-icons/fa6";
+import jsPDF from "jspdf";
 
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
@@ -16,6 +16,7 @@ export default function LandPage() {
       smooth: "easeInOutQuart",
     });
   };
+
   return (
     <div className="lanPageContainer" id="start">
       <div id="loader-wrapper">
@@ -45,7 +46,14 @@ export default function LandPage() {
           Hi, my name is <span className="namePurple">Uriel Bengaev</span>
         </h1>
         <h1>i'm Full stack developer</h1>
-        <a className="cvButton">Download my CV</a>
+        <a
+          href="https://eu-central.storage.cloudconvert.com/tasks/18a26c15-abc7-4c47-88db-58ba0defff6b/cvUrielPerg.webp?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cloudconvert-production%2F20240330%2Ffra%2Fs3%2Faws4_request&X-Amz-Date=20240330T212254Z&X-Amz-Expires=86400&X-Amz-Signature=781200335b8c6dbc18c8d8aaad0aec6a948771bf87b67afc4068cabb15c9a3e8&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22cvUrielPerg.webp%22&response-content-type=image%2Fwebp&x-id=GetObject"
+          className="cvButton"
+          download="Uriel-Benagev.jpg"
+        >
+          Download my CV
+        </a>
+
         <span className="letText">Let me show you...</span>
       </div>
 

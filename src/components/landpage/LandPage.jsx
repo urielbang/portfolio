b59223f15b7819f127aed3fff50a61bf6cb3b1ab
urielbang <uri32655@gmail.com>
2024-03-31 10:33:55 +0300

    download to pdf

--- src/components/landpage/LandPage.jsx
+++ src/components/landpage/LandPage.jsx
@@ -1,7 +1,7 @@
 import NavBar from "../navbar/NavBar";
 import Spline from "@splinetool/react-spline";
 import { load } from "./loaded";
-import jsPDF from "jspdf";
+import { saveAs } from "file-saver";
 
 import { Link as ScrollLink, animateScroll as scroll } from "react-scroll";
 
@@ -47,9 +47,9 @@ export default function LandPage() {
         </h1>
         <h1>i'm Full stack developer</h1>
         <a
-          href="https://eu-central.storage.cloudconvert.com/tasks/18a26c15-abc7-4c47-88db-58ba0defff6b/cvUrielPerg.webp?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=cloudconvert-production%2F20240330%2Ffra%2Fs3%2Faws4_request&X-Amz-Date=20240330T212254Z&X-Amz-Expires=86400&X-Amz-Signature=781200335b8c6dbc18c8d8aaad0aec6a948771bf87b67afc4068cabb15c9a3e8&X-Amz-SignedHeaders=host&response-content-disposition=attachment%3B%20filename%3D%22cvUrielPerg.webp%22&response-content-type=image%2Fwebp&x-id=GetObject"
+          href="cvUrielPerg.pdf"
           className="cvButton"
-          download="Uriel-Benagev.jpg"
+          download="cvUrielPerg.pdf"
         >
           Download my CV
         </a>

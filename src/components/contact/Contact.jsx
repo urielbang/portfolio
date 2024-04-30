d4038c95657808a8ac80b6a936ac5bff42b9e208
urielbang <uri32655@gmail.com>
2024-04-30 11:17:31 +0300

    full res

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -21,7 +21,10 @@ export default function Contact() {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    await axios.post("https://uriel-protfolio.onrender.com/api/v1/contact", contactData);
+    await axios.post(
+      "https://uriel-protfolio.onrender.com/api/v1/contact",
+      contactData
+    );
 
     setInputValue({ name: "", email: "", phone: "", message: "" });
   };
@@ -40,17 +43,15 @@ export default function Contact() {
       <div className="row">
         <h4
           className="headerTxtHear"
-          data-aos="fade-zoom-in"
-          data-aos-easing="ease-in-back"
-          data-aos-delay="300"
-          data-aos-offset="0"
+          data-aos="fade-up"
+          data-aos-duration="3000"
           style={{ textAlign: "center" }}
         >
           I’d love to hear from you!
         </h4>
       </div>
       <div className="row input-container">
-        <div data-aos="fade-right">
+        <div>
           <div className="styled-input wide">
             <input
               type="text"
@@ -62,7 +63,7 @@ export default function Contact() {
             <label>Name</label>
           </div>
         </div>
-        <div className="col-md-6 col-sm-12" data-aos="fade-left">
+        <div className="col-md-6 col-sm-12">
           <div className="styled-input">
             <input
               type="email"

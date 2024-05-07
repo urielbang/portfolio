b61455d6c1fb96d1fdd0e1f159c2e1f6008231bf
urielbang <uri32655@gmail.com>
2024-05-07 16:05:13 +0300

    SpeedInsights

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import App from "./App.jsx";
 import "./index.css";
 

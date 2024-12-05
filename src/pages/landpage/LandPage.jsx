dfbcfd0a3e329a789695ef4e4f082240f0c72803
urielbang <uri32655@gmail.com>
2024-12-05 10:55:26 +0200

    add background animation

--- src/pages/landpage/LandPage.jsx
+++ src/pages/landpage/LandPage.jsx
@@ -13,65 +13,90 @@ export default function LandPage() {
   };
 
   return (
-    <div className="lanPageContainer" id="start">
-      <NavBar />
-      <div className="timeline" data-aos="fade-right">
-        <div className="bullet" data-aos="fade-right" />
-        <ScrollLink
-          data-aos="fade-right"
-          to="work"
-          className="scroll-bottom"
-          smooth={true}
-          duration={800}
-          onClick={scrollToWork}
-        >
-          <img className="scroll-img" src={svg} alt="" data-aos="fade-right" />
-        </ScrollLink>
+    <>
+      <div className="context">
+        <>
+          <div className="lanPageContainer" id="start">
+            <NavBar />
+            <div className="timeline" data-aos="fade-right">
+              <div className="bullet" data-aos="fade-right" />
+              <ScrollLink
+                data-aos="fade-right"
+                to="work"
+                className="scroll-bottom"
+                smooth={true}
+                duration={800}
+                onClick={scrollToWork}
+              >
+                <img
+                  className="scroll-img"
+                  src={svg}
+                  alt=""
+                  data-aos="fade-right"
+                />
+              </ScrollLink>
+            </div>
+
+            <div className="containerText" data-aos="fade-right">
+              <a>
+                {`Start`}
+                <span>{`/>`}</span>
+              </a>
+
+              <h1>
+                Hi, my name is <span className="namePurple">Uriel Bengaev</span>
+              </h1>
+              <h1>
+                I'm
+                <Typewriter
+                  words={[
+                    " a Frontend Developer",
+                    " a React Developer",
+                    " a Full Stack Developer",
+                    " a Creative Programmer",
+                    " a Backend Developer",
+                  ]}
+                  loop={true}
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={150}
+                  deleteSpeed={100}
+                  delaySpeed={1500}
+                />
+              </h1>
+              <a
+                href="cvUrielPerg.pdf"
+                className="cvButton"
+                download="Uriel-Bengaev.CV.pdf"
+              >
+                Download my CV
+              </a>
+
+              <span className="letText">Let me show you...</span>
+            </div>
+
+            <div className="containerAnimation">
+              <div className="computerAnimation">
+                <Spline scene="https://prod.spline.design/Tt1-9L8VAmUjF5QR/scene.splinecode" />
+              </div>
+            </div>
+          </div>
+        </>
       </div>
-
-      <div className="containerText" data-aos="fade-right">
-        <a>
-          {`Start`}
-          <span>{`/>`}</span>
-        </a>
-
-        <h1>
-          Hi, my name is <span className="namePurple">Uriel Bengaev</span>
-        </h1>
-        <h1>
-          I'm
-          <Typewriter
-            words={[
-              " a Frontend Developer",
-              " a React Developer",
-              " a Full Stack Developer",
-              " a Creative Programmer",
-              " a Backend Developer",
-            ]}
-            loop={true}
-            cursor
-            cursorStyle="|"
-            typeSpeed={150}
-            deleteSpeed={100}
-            delaySpeed={1500}
-          />
-        </h1>
-        <a
-          href="cvUrielPerg.pdf"
-          className="cvButton"
-          download="Uriel-Bengaev.CV.pdf"
-        >
-          Download my CV
-        </a>
-
-        <span className="letText">Let me show you...</span>
+      <div className="area">
+        <ul className="circles">
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+          <li />
+        </ul>
       </div>
-
-      <div className="containerAnimation">
-        <div className="computerAnimation">
-          <Spline scene="https://prod.spline.design/Tt1-9L8VAmUjF5QR/scene.splinecode" />
-        </div>
-      </div>
-    </div>
+    </>
   );
 }
